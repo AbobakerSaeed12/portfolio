a1b524d43ecc3ae78bba2f3302bb8fd4303aa566
Abobaker Saeed <abobakersaeed123@gmail.com>
2022-06-21 14:42:36 -0500

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+// gsap.from(".transition", {
+//   scrollTrigger: {
+//     trigger: ".transition",
+//     start: "center bottom",
+//   },
+//   duration: 1.2,
+//   y: 50,
+//   opacity: 0,
+//   stagger: 0.3,
+// });
