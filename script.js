9f05de98318bd0a42e8b336ecf6a50651a04e7b1
abobaker saeed <abobakersaed123@gmail.com>
2022-06-28 16:52:40 -0500

    desktop design

---
+++ script.js
@@ -0,0 +1,11 @@
+// gsap.from(".transition", {
+//   scrollTrigger: {
+//     trigger: ".transition",
+//     start: "center bottom",
+//   },
+//   duration: 1.2,
+//   y: 50,
+//   opacity: 0,
+//   stagger: 0.3,
+// });
+// alert("still working on it");
